bd7b290e6a342954843f7cd29e81eeaa11fbbfa7
kyle-peterson-1 <pet23003@byui.edu>
2024-07-22 22:23:45 -0700

    W 05 updates

--- scripts/form.js
+++ scripts/form.js
@@ -1,27 +1,27 @@
 const products = [
     {
         'id': 'fc-1888',
-        'name': 'flux capacitor',
+        'name': 'do hicky',
         'averageRating': 4.5
     },
     {
         'id': 'fc-2050',
-        'name': 'power laces',
+        'name': 'go-go gadjit ',
         'averageRating': 4.7
     },
     {
         'id': 'fs-1987',
-        'name': 'time circuits',
+        'name': 'time mechine',
         'averageRating': 3.5
     },
     {
         'id': 'ac-2000',
-        'name': 'low voltage reactor',
+        'name': 'flux capacitor',
         'averageRating': 3.9
     },
     {
         'id': 'jj-1969',
-        'name': 'warp equalizer',
+        'name': 'smart phone',
         'averageRating': 5.0
     }
 ];
